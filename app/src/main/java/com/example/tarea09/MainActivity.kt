a059dff3a694d529665f6b33f33e98697e5dc15f
CesarNunez1506 <cesar.nunez@tecsup.edu.pe>
2024-10-06 19:31:28 -0500

    Fix: Correcion de errores de ejecucion

--- app/src/main/java/com/example/tarea09/MainActivity.kt
+++ app/src/main/java/com/example/tarea09/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.tarea09
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.material3.MaterialTheme.colorScheme
 import com.example.tarea09.ui.PokemonListScreen
-import java.lang.reflect.Modifier
+import com.example.tarea09.ui.theme.MyPokemonAppTheme
+import androidx.compose.ui.Modifier // Este es el import correcto
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
