4ea75e883c0149ba81b01d26cb1fcd905631f610
CesarNunez1506 <cesar.nunez@tecsup.edu.pe>
2024-10-06 19:22:16 -0500

    Chore: MainActivity

--- app/src/main/java/com/example/tarea09/MainActivity.kt
+++ app/src/main/java/com/example/tarea09/MainActivity.kt
@@ -1,47 +1,25 @@
 package com.example.tarea09
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tarea09.ui.theme.Tarea09Theme
+import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme.colorScheme
+import com.example.tarea09.ui.PokemonListScreen
+import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Tarea09Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MyPokemonAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = colorScheme.background
+                ) {
+                    PokemonListScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Tarea09Theme {
-        Greeting("Android")
     }
 }
